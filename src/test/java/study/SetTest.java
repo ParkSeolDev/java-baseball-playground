d940788ddb45406265afcba0843a26ef7fcbf44a
ParkSeolDev <parkseol.dev@gmail.com>
2024-03-06 23:26:35 +0900

    step0 - Set Collection 요구사항1

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,32 @@
+package study;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SetTest {
+	private Set<Integer> numbers;
+
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+
+	@Test
+	void set_size() {
+		assertThat(numbers.size()).isEqualTo(3);
+	}
+
+	
+}
